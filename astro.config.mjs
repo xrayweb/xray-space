8ed4a9d3d583a0249f2bd6981f4c87187c520421
Ian Whitfield <ian@xrayweb.co.uk>
2023-12-27 16:34:34 +0000

    Added github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://xray-space.github.io',
+	base: '/xrayweb/xray-space',
 	integrations: [
 		starlight({
 			title: 'My Docs',
